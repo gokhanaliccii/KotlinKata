21a016885e883eecf557c9904100f75a83006092
Gokhan Alici <gokhanaliccii@gmail.com>
2021-04-08 23:13:10 +0100

    Add sample test case

---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class SampleTest{
+
+    @Test
+    fun sampleTest(){
+        assertEquals(1 ,1 )
+    }
+}
